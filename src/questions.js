eb1ca1ee05f97de2b67da95ac1bc7f28c0aa9287
dophuc0111001 <dophuc0111001@gmail.com>
2023-01-12 14:43:30 +0700

    first upload

---
+++ src/questions.js
@@ -0,0 +1,214 @@
+export const questions = [
+  {
+    questionText: "Sắp xếp các số 0, 5, 2, 10 theo thứ tự từ bé đến lớn",
+    answers: [
+      {
+        answerText: "A. 2, 0, 10, 5",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 2, 5, 0, 10",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 0, 2, 5, 10",
+        correctAnswer: true,
+      },
+      {
+        answerText: "D. 10, 5, 2, 0",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Phép tính nào sai?",
+    answers: [
+      {
+        answerText: "A. 7 - 5 = 2",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 4 + 4 = 9",
+        correctAnswer: true,
+      },
+      {
+        answerText: "C. 10 - 9 = 1",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 0 + 0 = 0",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Có mấy số có 1 chữ số",
+    answers: [
+      {
+        answerText: "A. 10",
+        correctAnswer: true,
+      },
+      {
+        answerText: "B. 11",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 9",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 8",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Kết quả của phép tính: 10 - 8 + 3 - 2 = ?",
+    answers: [
+      {
+        answerText: "A. 1",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 2",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 3",
+        correctAnswer: true,
+      },
+      {
+        answerText: "D. 5",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Hiệu của hai số 73 và 37",
+    answers: [
+      {
+        answerText: "A. 46",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 35",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 47",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 36",
+        correctAnswer: true,
+      },
+    ],
+  },
+  {
+    questionText: "Hồng có 22 quyển truyện tranh. Lan lấy bớt của Hồng 5 quyển. Số truyện còn lại của Hồng là: ",
+    answers: [
+      {
+        answerText: "A. 27",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 17",
+        correctAnswer: true,
+      },
+      {
+        answerText: "C. 22",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 15",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Điền số thích hợp vào chỗ trống: 60 cm = ......dm",
+    answers: [
+      {
+        answerText: "A. 6 dm",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 6",
+        correctAnswer: true,
+      },
+      {
+        answerText: "C. 60",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 6 cm",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText:
+      "Trong một ngày, cửa hàng bán được 56 kg đường, trong đó buổi sáng bán được 27kg. Hỏi buổi chiều bán được bao nhiêu kg đường?",
+    answers: [
+      {
+        answerText: "A. 83",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 39",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 29",
+        correctAnswer: true,
+      },
+      {
+        answerText: "D. 43",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Trong các số dưới đây, số nào không thuộc dãy số: 1, 4, 7, 10, 13,...",
+    answers: [
+      {
+        answerText: "A. 1000",
+        correctAnswer: false,
+      },
+      {
+        answerText: "B. 1234",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 2007",
+        correctAnswer: true,
+      },
+      {
+        answerText: "D. 100",
+        correctAnswer: false,
+      },
+    ],
+  },
+  {
+    questionText: "Dùng 4 chữ số lẻ: 1, 3, 5, 7 để viết tất cả các số có 4 chữ số khác nhau thì viết được bao nhiêu số",
+    answers: [
+      {
+        answerText: "A. 24",
+        correctAnswer: true,
+      },
+      {
+        answerText: "B. 30",
+        correctAnswer: false,
+      },
+      {
+        answerText: "C. 18",
+        correctAnswer: false,
+      },
+      {
+        answerText: "D. 12",
+        correctAnswer: false,
+      },
+    ],
+  },
+];
